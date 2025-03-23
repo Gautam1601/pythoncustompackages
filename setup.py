48b2ad999ca3b8bb66420f364822c74c6c28d32e
Gautam <113298649+Gautam1601@users.noreply.github.com>
2025-03-23 07:39:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="anythingoes",
     version="0.0.4",
-    author="Gautam",
+    author="Gautam1601",
     author_email="gautamsr101@gmail.com",
     url="https://www.youtube.com/channel/UCv9MUffHWyo2GgLIDLVu0KQ",
     description="An application that informs you of the time in different locations and timezones",
